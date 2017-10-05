a134b152e1185df32f7e344f11c070c83fc02ae6
Eugen Behm <behm.eugen@googlemail.com>
2017-10-05 19:42:32 +0200

    CHANGE: Welcome View angepasst.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Neues Projekt
                 </div>
 
                 <div class="links">
